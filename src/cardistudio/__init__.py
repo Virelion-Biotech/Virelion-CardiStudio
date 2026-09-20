28da59eb8deab17c5057ad7e0237ccef6efded75
Syed Umer Hannan <syedumerhannan@icloud.com>
2026-09-20 16:24:51 +0300

    fix: derive package version from installed metadata

--- src/cardistudio/__init__.py
+++ src/cardistudio/__init__.py
@@ -1,13 +1,13 @@
 """CardiStudio: reproducible cardiac experimental-population design and simulation."""
-from .models import ChallengeSpec, FeatureSpec, PopulationSpec
-from .population import PopulationBuilder, Population
+from .models import ChallengeSpec, FeatureSpec, PopulationSpec, CURRENT_CHALLENGE_VERSION
+from .population import PopulationBuilder, Population, GENERATOR_VERSION
 from .validation import ValidationReport, validate_challenge, validate_population
 from .analysis import summarize_population, balance_report
 from .io import load_challenge, save_challenge, save_population, load_population
 from .design import Factor, ExperimentalDesign, full_factorial
-from .correlations import gaussian_copula, correlated_normals
-from .trajectory import TrajectorySpec, exponential_recovery, logistic_transition
-from .constraints import Constraint, ConstraintEngine, ConstraintReport, range_constraint, relationship_constraint
+from .correlations import gaussian_copula, correlated_normals, validate_correlation
+from .trajectory import TrajectorySpec, exponential_recovery, logistic_transition, subject_exponential_long
+from .constraints import Constraint, ConstraintEngine, ConstraintReport, range_constraint, relationship_constraint, declarative_constraint
 from .power import cohens_d, approximate_two_sample_n
 
 __all__ = [
